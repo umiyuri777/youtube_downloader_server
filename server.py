1ae769f1144354782e3f846c7f5d98c46462198a
umiyuri777 <yuseiai15@gmail.com>
2024-03-13 16:25:19 +0900

    Future型想定での通信ができた

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import subprocess
-from flask import Flask, request
+from flask import Flask, jsonify, request, make_response
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -17,28 +17,37 @@ def download_video():
 
     try:
         # yt-dlpコマンドを実行
-        result = subprocess.Popen(omakeurl, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+        result = subprocess.run(omakeurl, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
         print("ダウンロードを開始します...", flush=True)
 
-        while True:
-            # プロセスの出力をリアルタイムで読み取る
-            output = result.stdout.readline()   # 1行読み取る
-            if output == '' and result.returncode is not None:  # プロセスが終了していたらループを抜ける
-                break
-            if output:
-                yield output  # 1行返す
+        # while True:
+        #     # プロセスの出力をリアルタイムで読み取る
+        #     output = result.stdout.readline()   # 1行読み取る
+        #     if output == '' and result.returncode is not None:  # プロセスが終了していたらループを抜ける
+        #         break
+        #     if output:
+        #         yield output  # 1行返す
 
         # ダウンロードが正常に終了したかどうかを判定
         if result.returncode == 0:
             print("debug: Download successful", flush=True)
-            return 'Download successful'
+            response = {
+                'message': 'Download successful'
+            }
+            return make_response(jsonify(response))
         else:
             print(f'Error: {result.stderr}', flush=True)
-            return f'Error: {result.stderr}'
+            response = {
+                'message': f'Error: {result.stderr}'
+            }
+            return make_response(jsonify(response))
     except FileNotFoundError:
         # yt-dlpコマンドが見つからなかった場合
         print('Error: yt-dlp command not found', flush=True)
-        return 'Error: yt-dlp command not found'
+        response = {
+            'message': 'Error: yt-dlp command not found'
+        }
+        return make_response(jsonify(response))
 
 if __name__ == '__main__':
     app.debug = True
